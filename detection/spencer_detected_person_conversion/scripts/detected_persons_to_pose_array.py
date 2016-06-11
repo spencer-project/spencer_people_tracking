f4650576cbae95029108c06d2d8623685da5b563
Timm Linder <linder@cs.uni-freiburg.de>
2016-06-11 13:26:58 +0200

    Adding licenses for spencer_detected_person_conversion

--- detection/spencer_detected_person_conversion/scripts/detected_persons_to_pose_array.py
+++ detection/spencer_detected_person_conversion/scripts/detected_persons_to_pose_array.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2015, Timm Linder, Social Robotics Lab, University of Freiburg
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 """
 Converts spencer_tracking_msgs/DetectedPersons into a geometry_msgs/PoseArray. This conversion is lossy,
 as covariance matrix, modality, detection ID and confidence are omitted during the conversion.
@@ -27,6 +56,6 @@ outputTopic = rospy.resolve_name("/pose_array")
 
 sub = rospy.Subscriber(inputTopic, DetectedPersons, newMessageReceived, queue_size=5)
 pub = rospy.Publisher(outputTopic, PoseArray, queue_size=5)
-    
+
 rospy.loginfo("Re-publishing spencer_tracking_msgs/DetectedPersons from %s as geometry_msgs/PoseArray at %s" % (inputTopic, outputTopic) )
 rospy.spin()
--- detection/spencer_detected_person_conversion/scripts/position_measurement_array_to_detected_persons.py
+++ detection/spencer_detected_person_conversion/scripts/position_measurement_array_to_detected_persons.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python
 
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2015, Timm Linder, Social Robotics Lab, University of Freiburg
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 """
 Converts a people_msgs/PositionMeasurementArray into spencer_tracking_msgs/DetectedPersons which can be processed using a tracker, or visualized using
 spencer_tracking_rviz_plugin. This conversion is lossless except for the co-occurrence matrix and the object identifier string.
@@ -27,7 +55,7 @@ def newMessageReceived(positionMeasurementArray):
         detectedPerson.modality = positionMeasurement.name;
         detectedPerson.confidence = positionMeasurement.reliability
         detectedPerson.pose.pose.position = positionMeasurement.pos
-        
+
         # Covariance
         for x in xrange(0, 3):
             for y in xrange(0, 3):
@@ -46,7 +74,7 @@ def newMessageReceived(positionMeasurementArray):
             detectionId += detectionIdIncrement
 
         detectedPersons.detections.append(detectedPerson)
-        
+
     pub.publish(detectedPersons)
 
 
