05c83fe8631b83300b17d009be524108090c4b60
ruffsl <roxfoxpox@gmail.com>
2017-05-02 21:20:13 -0700

    Adding simple cv2 face detecter

--- classification/face_classifier/openface_classifier/src/main.py
+++ classification/face_classifier/openface_classifier/src/main.py
@@ -19,6 +19,7 @@ from spencer_tracking_msgs.msg import DetectedPersons, DetectedPerson
 
 import message_filters
 
+faceCascade = cv2.CascadeClassifier('/opt/ros/kinetic/share/OpenCV-3.2.0-dev/haarcascades/haarcascade_frontalface_default.xml')
 
 class image_converter:
 
@@ -31,7 +32,7 @@ class image_converter:
     # self.roi_sub = rospy.Subscriber("/spencer/perception_internal/people_detection/rgbd_front_top/upper_body_detector/detections",UpperBodyDetector,self.callback)
 
         image_sub = \
-            message_filters.Subscriber('/spencer/perception_internal/people_detection/rgbd_front_top/upper_body_detector/image'
+            message_filters.Subscriber('/spencer/sensors/rgbd_front_top/rgb/image_rect_color'
                 , Image)
         detections_sub = \
             message_filters.Subscriber('/spencer/perception_internal/people_detection/rgbd_front_top/upper_body_detector/detections'
@@ -62,6 +63,21 @@ class image_converter:
             h = detections.height[0]
 
             cv_image_crop = cv_image[y:y + h, x:x + w]
+
+            gray = cv2.cvtColor(cv_image_crop, cv2.COLOR_BGR2GRAY)
+
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=3,
+                minSize=(50, 50),
+                flags = cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(cv_image_crop, (int(x-w*0.1), int(y-h*0.1)), (x+int(w*1.1), y+int(h*1.1)), (0, 255, 0), 2)
+
 
             person = persons.detections[0]
             font = cv2.FONT_HERSHEY_SIMPLEX
