f47a22e890718d9807c070ca54330c0931d62053
ruffsl <roxfoxpox@gmail.com>
2017-05-07 20:41:50 -0700

    Adding track syn to image cropping works, but only for a short amount of time feels like a buffer mismatch

--- classification/face_classifier/openface_classifier/src/main.py
+++ classification/face_classifier/openface_classifier/src/main.py
@@ -33,7 +33,7 @@ class image_converter:
 
     def __init__(self):
         self.image_pub = rospy.Publisher('image_tracked', Image, queue_size=10)
-        self.track_pub = rospy.Publisher('persons_tracked', Image, queue_size=10)
+        self.track_pub = rospy.Publisher('persons_tracked', TrackedPersons, queue_size=100)
 
         self.bridge = CvBridge()
 
@@ -43,12 +43,15 @@ class image_converter:
         detections_sub = \
             message_filters.Subscriber('/spencer/perception_internal/people_detection/rgbd_front_top/upper_body_detector/detections'
                 , UpperBodyDetector)
-        persons_sub = \
+        detected_persons_sub = \
             message_filters.Subscriber('/spencer/perception_internal/detected_persons/rgbd_front_top/upper_body'
                 , DetectedPersons)
+        tracked_persons_sub = \
+            message_filters.Subscriber('persons_tracked'
+                , TrackedPersons)
 
         self.time_sync = message_filters.ApproximateTimeSynchronizer(
-            [image_sub, detections_sub, persons_sub], 30, 1)
+            [image_sub, detections_sub, detected_persons_sub, tracked_persons_sub], 30, 1)
         self.time_sync.registerCallback(self.time_sync_callback)
 
         track_sync_sub = message_filters.Subscriber('/spencer/perception_internal/detected_persons/rgbd_front_top/upper_body', DetectedPersons)
@@ -59,64 +62,92 @@ class image_converter:
         self,
         image,
         detections,
-        persons,
+        detected_persons,
+        tracked_persons,
         ):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(image, 'bgr8')
         except CvBridgeError, e:
             print(e)
+        print('time_sync_callback')
 
-        try:
-            x = detections.pos_x[0]
-            y = detections.pos_y[0]
-            w = detections.width[0]
-            h = detections.height[0]
+        detectionIndexLookup = {}
+        for i, detected_person in enumerate(detected_persons.detections):
+            detectionIndexLookup[detected_person.detection_id] = i
+        trackLookups = {}
+        for track in tracked_persons.tracks:
+            track_id = track.track_id
+            detection_id = track.detection_id
+            try:
+                index = detectionIndexLookup[detection_id]
+                trackLookup = {'track_id': track_id, 'index':index}
+                trackLookups[track.detection_id] = trackLookup
+            except:
+                pass
 
-            cv_image_crop = cv_image[y:y + h, x:x + w]
+        if not trackLookups:
+            print('No track.detection_id found in detected_persons.detections')
+        else:
+            for detection_id in trackLookups:
+                trackLookup = trackLookups[detection_id]
+                index = trackLookup['index']
+                track_id = trackLookup['track_id']
 
-            gray = cv2.cvtColor(cv_image_crop, cv2.COLOR_BGR2GRAY)
+                print('detection_id: {} track_id: {}'.format(detection_id, track_id))
+                x = detections.pos_x[index]
+                y = detections.pos_y[index]
+                w = detections.width[index]
+                h = detections.height[index]
 
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1,
-                minNeighbors=3,
-                minSize=(50, 50),
-                flags = cv2.CASCADE_SCALE_IMAGE
-            )
-
-            # Draw a rectangle around the faces
-            for (x, y, w, h) in faces:
-                cv2.rectangle(cv_image_crop, (int(x-w*0.1), int(y-h*0.1)), (x+int(w*1.1), y+int(h*1.1)), (0, 255, 0), 2)
-
-
-            person = persons.detections[0]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(
-                cv_image_crop,
-                str(person.detection_id%10000000),
-                (0, h),
-                font,
-                1*w/160.0,
-                (255, 255, 255),
-                2,
+                cv_image_crop = cv_image[y:y + h, x:x + w]
+                gray = cv2.cvtColor(cv_image_crop, cv2.COLOR_BGR2GRAY)
+                faces = faceCascade.detectMultiScale(
+                    gray,
+                    scaleFactor=1.1,
+                    minNeighbors=3,
+                    minSize=(50, 50),
+                    flags = cv2.CASCADE_SCALE_IMAGE
                 )
 
-            try:
-                self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image_crop,
-                                       'bgr8'))
-            except CvBridgeError, e:
-                print(e)
-        except:
-            pass
+                # Draw a rectangle around the faces
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(cv_image_crop, (int(x-w*0.1), int(y-h*0.1)), (x+int(w*1.1), y+int(h*1.1)), (0, 255, 0), 2)
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(
+                    cv_image_crop,
+                    str(track_id%10000000),
+                    (0, h),
+                    font,
+                    1*w/160.0,
+                    (255, 255, 255),
+                    2,
+                    )
+
+                try:
+                    self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image_crop,
+                                           'bgr8'))
+                except CvBridgeError, e:
+                    print(e)
+
+                break
 
     def track_sync_callback(
         self,
         trackAssociation,
-        persons):
-        person = persons.detections[0]
-        person_id = person.detection_id
-        tracking_id = trackAssociation.lookupTrackId(person_id)
-        print('person_id: {} tracking_id: {}'.format(person_id, tracking_id))
+        detected_persons):
+        tracked_persons = TrackedPersons()
+        tracked_persons.header = detected_persons.header
+        for detected_person in detected_persons.detections:
+            detection_id = detected_person.detection_id
+            track_id = trackAssociation.lookupTrackId(detection_id)
+            if track_id is not None:
+                # print('detection_id: {} track_id: {}'.format(detection_id, track_id))
+                tracked_person = TrackedPerson()
+                tracked_person.track_id = track_id
+                tracked_person.detection_id = detection_id
+                tracked_persons.tracks.append(tracked_person)
+        self.track_pub.publish(tracked_persons)
 
 def main(args):
     ic = image_converter()
@@ -126,7 +157,6 @@ def main(args):
     except KeyboardInterrupt:
         print('Shutting down')
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main(sys.argv)
