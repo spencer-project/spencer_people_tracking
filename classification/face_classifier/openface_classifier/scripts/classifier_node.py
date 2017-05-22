33dc5ae5f723c4c24221904ef0916dcb3eb5ebe7
jaskaran1 <jaskaranvirdi1@gmail.com>
2017-05-21 23:53:51 -0700

    indentation change

--- classification/face_classifier/openface_classifier/scripts/classifier_node.py
+++ classification/face_classifier/openface_classifier/scripts/classifier_node.py
@@ -17,52 +17,52 @@ classifier_path = 'classifier.pkl'
 features_file = 'features.npy'
 
 class Classifier:
-	def __init__(self):
-		rospy.Subscriber("input",,track_feature_msg_callback) #subscribes to (track,feature) message
-		self.track_person_assoc_pub = rospy.Publisher('output', TrackIdentityAssociations, queue_size=10)
-		self.server = DynamicReconfigureServer(ConfigType, self.reconfigure_classifier_callback)
-		self.lock = threading.lock()
+    def __init__(self):
+        rospy.Subscriber("input",,track_feature_msg_callback) #subscribes to (track,feature) message
+        self.track_person_assoc_pub = rospy.Publisher('output', TrackIdentityAssociations, queue_size=10)
+        self.server = DynamicReconfigureServer(ConfigType, self.reconfigure_classifier_callback)
+        self.lock = threading.lock()
 
 
-	def load_model(self, classifier_path):
-		with open(classifier_path, 'r') as f:
+    def load_model(self, classifier_path):
+        with open(classifier_path, 'r') as f:
             clf = pickle.load(f)
         self.clf = clf
 
     def load_features(self, feature_path):
-    	self.features = np.load(feature_path)
-	
-	def reconfigure_classifier_callback(self, config, level):
+        self.features = np.load(feature_path)
+    
+    def reconfigure_classifier_callback(self, config, level):
         with self.lock:
-        	self.load_model(config['classifier_path'])
-        	self.load_features(config['feature_path'])
+            self.load_model(config['classifier_path'])
+            self.load_features(config['feature_path'])
         return config
 
-	def track_feature_msg_callback(self,data):
-		#subscriber callback
-		#Case when only single track and feature is there
-		# track_id = data.track_id #int
-		# feature = data.feature #array of floats
-		# label = self.clf.predict(feature)[0]
+    def track_feature_msg_callback(self,data):
+        #subscriber callback
+        #Case when only single track and feature is there
+        # track_id = data.track_id #int
+        # feature = data.feature #array of floats
+        # label = self.clf.predict(feature)[0]
 
-		#Case when multiple tracks and features are there
-		track_assocs = TrackIdentityAssociations()
+        #Case when multiple tracks and features are there
+        track_assocs = TrackIdentityAssociations()
         track_assocs.header = data.header
         track_list = data.tracks
-		features = data.features
+        features = data.features
 
-		with self.lock:
-			for (track,feature) in zip(track_list,features):
-				track_assoc = TrackIdentityAssociation()
-				label = self.clf.predict(feature)[0]
-				track_assoc.track_id = track
-				track_assoc.detection_id = track.detection_id
-				track_assoc.person_name = label
-				track_assocs.tracks.append(track_assoc)
-			self.track_person_assoc_pub.publish(track_assocs)
+        with self.lock:
+            for (track,feature) in zip(track_list,features):
+                track_assoc = TrackIdentityAssociation()
+                label = self.clf.predict(feature)[0]
+                track_assoc.track_id = track
+                track_assoc.detection_id = track.detection_id
+                track_assoc.person_name = label
+                track_assocs.tracks.append(track_assoc)
+            self.track_person_assoc_pub.publish(track_assocs)
 
 
 if __name__ == '__main__':
-	rospy.init_node('classifier_node')
-	classifier_object = Classifier()
-	rospy.spin()
+    rospy.init_node('classifier_node')
+    classifier_object = Classifier()
+    rospy.spin()
