e3972c599610a5a10bfd94d5cbbcead6ab5cdd30
jaskaran1 <jaskaranvirdi1@gmail.com>
2017-05-30 15:42:54 -0700

    track label associator node logic added with tracklabelassoc service logic

--- classification/face_classifier/openface_classifier/scripts/data_associator_node.py
+++ classification/face_classifier/openface_classifier/scripts/data_associator_node.py
@@ -8,52 +8,65 @@ from openface_classifier.msg import (
 	labels2FeaturesGoal,
 	LabelToFeatures
 )
+from spencer_tracking_msgs.msg import (
+    TrackedPerson,
+    TrackedPersons,
+    TrackIdentityAssociation,
+    TrackIdentityAssociations
+)
 from spencer_vision_msgs.msg import (
 	PersonEmbedding,
 	PersonEmbeddings,
 )
-import random
+from openface_classifier.srv import trackLabelAssociation
+from collections import defaultdict
 
-def make_rand_vector(dims):
-	vec = [random.gauss(0, 1) for i in range(dims)]
-	mag = sum(x**2 for x in vec) ** .5
-	return [x/mag for x in vec]
+class DataAssociator:
 
-# class DataAssociator:
+	def __init__(self):
+		rospy.Subscriber("track_feature_input", PersonEmbeddings, self.track_feature_msg_callback) 		#subscribes to (track,feature) message
+		rospy.Service('track_label', trackLabelAssociation, self.track_label_service_callback)
+		self.actionlibClient = actionlib.SimpleActionClient('labels2Features', labels2FeaturesAction)
+		self.actionlibClient.wait_for_server()
 
-# 	def __init__(self):
-# 		self.track_feature_sub = rospy
-# 		self.client = actionlib.SimpleActionClient('labels2Features', labels2FeaturesAction)
+	def track_feature_msg_callback(self, personEmbeddings):
+		elements = personEmbeddings.elements
+		features = []
+		
+		for element in elements:
+			elem_track_id = element.track_id
+			elem_embedding = element.embedding
+			if elem_track_id == self.track_id:
+				features.append(elem_embedding)
+
+		if len(features)==0:
+			return
+		
+		goal = labels2FeaturesGoal()
+		
+		label2Features_msg = LabelToFeatures()
+		label2Features_msg.person_name = self.track_label
+		label2Features_msg.reset_label = False
+		label2Features_msg.person_embeddings = PersonEmbeddings()
+
+		for embedding in features:
+			person_embedding = PersonEmbedding()
+			person_embedding.embedding = embedding
+			person_embedding.track_id = self.track_id
+			label2Features_msg.person_embeddings.elements.append(person_embedding)
+
+		goal.elements.append(label2Features_msg)
+		self.actionlibClient.send_goal(goal)
+		self.actionlibClient.wait_for_result(rospy.Duration.from_sec(10.0))
+		
+	def track_label_service_callback(self, trackIdentityAssociationServiceRequest):
+		self.track_id = trackIdentityAssociationServiceRequest.track_id
+		self.track_label = trackIdentityAssociationServiceRequest.person_name
+		return trackLabelAssociationResponse(True)
 
 
 '''This node collects Track->Label and Track->Feature mappings, gets Label->Feature mappings from them and sends it to the trainer_node'''
 if __name__ == '__main__':
 	rospy.init_node('data_associator_node')
-	# data_associator_object = DataAssociator()
-	# rospy.spin()
-	client = actionlib.SimpleActionClient('labels2Features', labels2FeaturesAction)
-	client.wait_for_server()
-
-	goal = labels2FeaturesGoal()
-	# Fill in the goal here
-	num_labels_to_send = 3
-	label_list = ['Jaskaran','Ruffin','Shengye','Priyam','Henrik']
-	for i in xrange(num_labels_to_send):
-		label2Features_msg = LabelToFeatures()
-		label2Features_msg.person_name = random.choice(label_list)
-		
-		label2Features_msg.person_embeddings = PersonEmbeddings()
-
-		num_embeddings = 1
-		for i in range(num_embeddings):
-			person_embedding = PersonEmbedding()
-			embedding = make_rand_vector(128)
-			person_embedding.embedding = embedding
-
-			label2Features_msg.person_embeddings.elements.append(person_embedding)
-
-		label2Features_msg.reset_label = False
-		goal.elements.append(label2Features_msg)
-	# print goal
-	client.send_goal(goal)
-	client.wait_for_result(rospy.Duration.from_sec(5.0))
+	data_associator_object = DataAssociator()
+	rospy.spin()
